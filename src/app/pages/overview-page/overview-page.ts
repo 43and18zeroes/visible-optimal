d8dcd28a0db2e114f86ce0d9909091b459f7bc27
Christoph <c.wagner.germany@gmail.com>
2025-08-31 15:54:00 +0200

    data

--- src/app/pages/overview-page/overview-page.ts
+++ src/app/pages/overview-page/overview-page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { F_AMOUNTS } from '../../constants';
+import { DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-overview-page',
-  imports: [],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './overview-page.html',
   styleUrl: './overview-page.scss'
 })
 export class OverviewPage {
 
+  amounts = F_AMOUNTS;
+
+  purchaseDate = new Date(this.amounts.PURCHASE_DATE);
 }
