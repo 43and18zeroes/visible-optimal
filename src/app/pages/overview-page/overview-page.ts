675906bcd031bf9f4c4de6e82d34d1d76ca0ada6
Christoph <c.wagner.germany@gmail.com>
2025-09-01 18:12:32 +0200

    copy

--- src/app/pages/overview-page/overview-page.ts
+++ src/app/pages/overview-page/overview-page.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { F_AMOUNTS } from '../../constants';
 import { DatePipe, CurrencyPipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-overview-page',
-  imports: [DatePipe, CurrencyPipe],
+  imports: [DatePipe, CurrencyPipe, MatIcon],
   templateUrl: './overview-page.html',
   styleUrl: './overview-page.scss'
 })
 export class OverviewPage {
-
   amounts = F_AMOUNTS;
-
   purchaseDate = new Date(this.amounts.PURCHASE_DATE);
+  copied = false;
+
+  async copyToClipboard(text: string) {
+    await navigator.clipboard.writeText(text);
+    this.copied = true;
+
+    setTimeout(() => {
+      this.copied = false;
+    }, 3000);
+  }
 }
