9a173fdd6cd5d1200367db9cce2dedb565c8f553
Christoph <c.wagner.germany@gmail.com>
2025-09-02 04:47:29 +0200

    de

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID, 
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common'; // ✅ Funktion importieren
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'de' },
+  ],
 };
